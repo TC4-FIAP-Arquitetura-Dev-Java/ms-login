a3d0f862b5eb79d87d28b5bc69a93b036f11f712
gu_roodri <ghustavo516@gmail.com>
2025-10-13 21:25:36 -0300

    feat: implementação inicial do oauth

--- src/main/java/com/ms/login/config/SecurityConfig.java
+++ src/main/java/com/ms/login/config/SecurityConfig.java
@@ -1,10 +1,27 @@
 package com.ms.login.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(authorizedConfig -> {
+            authorizedConfig.requestMatchers("/public").permitAll();
+            authorizedConfig.requestMatchers("/swagger-ui**").permitAll();
+            authorizedConfig.requestMatchers("/logout").permitAll();
+            authorizedConfig.anyRequest().authenticated();
+        })
+        .oauth2Login(Customizer.withDefaults())
+//        .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+        .build();
+    }
 
 }
--- src/main/java/com/ms/login/entrypoint/controllers/AuthController.java
+++ src/main/java/com/ms/login/entrypoint/controllers/AuthController.java
@@ -1,4 +1,25 @@
 package com.ms.login.entrypoint.controllers;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+
+@RestController
+//@RequestMapping("/v1/auth")
 public class AuthController {
+
+    @GetMapping("/public")
+    public String publicAuth() {
+        return "<h3>Rota Publica, aqui voce tem acesso </h3>";
+    }
+
+    @GetMapping("/private")
+    public String privateAuth(@AuthenticationPrincipal Principal principal) {
+        return String.format("<h3>Rota Privada, somente os com acesso entra aqui %s </h3>", principal);
+    }
+
+
 }
